77b93130afb4c5698332ca125834cc3b1ad0ec47
Alexander Bernauer <alex@copton.net>
2010-01-16 02:58:27 +0100

    local adaption

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-JAVA_DEV_ROOT=~/xtc
-CLASSPATH=~/xtc/classes:~/xtc/bin/junit.jar:~/xtc/bin/antlr.jar
-#CLASSPATH=~/xtc/classes
+XTC_PATH=`pwd`
+JAVA_DEV_ROOT=$XTC_PATH
+CLASSPATH=$XTC_PATH/classes:.:/usr/share/java/junit.jar:/usr/share/java/antlr.jar
 JAVA_HOME=/usr/bin
 PATH_SEP=:
-export JAVA_DEV_ROOT CLASSPATH JAVA_HOME PATH_SEP
+export JAVA_DEV_ROOT CLASSPATH JAVA_HOME PATH_SEP XTC_PATH
